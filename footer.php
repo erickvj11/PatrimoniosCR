5d32e0bdc68e285d1e42d559d4be24a1166643b4
Rita Robles Loaiza <rita.robles@labwebcr.com>
2018-09-08 09:42:08 -0600

    Inicio Proyecto - Base
    Proyecto Ajax

---
+++ footer.php
@@ -0,0 +1,20 @@
+<<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<title></title>
+</head>
+<body>
+
+	<!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Costa Rica Arquitectónica 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <?php include('header.php'); ?>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Patrimonios Arquitectónicos de Costa Rica</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/1-col-portfolio.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading -->
+      <h1 class="my-4">Descubre
+        <small>las maravillosas joyas arquitectónicas de Costa Rica</small>
+      </h1>
+
+      <!-- Project One -->
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img\San Jose.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>San José</h3>
+          <p>La arquitectura de carácter histórico, principalmente neoclásico, puede apreciarse en el Centro Histórico de San José que data de 1850 y tiene un perímetro de diez manzanas de ancho por diez manzanas de largo, el cual alberga los edificios y plazas más antiguos e importantes de la ciudad.</p>
+          <a class="btn btn-primary" href="#">Ver Más...</a>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <hr>
+
+      <!-- Project Two -->
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img\Alajuela.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Alajuela</h3>
+          <p>La mayoría de los sitios de interés cultural están ubicados en las cercanías del Parque Central. A sus costados se encuentran la catedral (Este), el Museo Histórico Cultural Juan Santamaría (Norte), el Centro Alajuelense de la Cultura (Oeste) y el llamado Edificio de Balcones (Sur).</p>
+          <a class="btn btn-primary" href="#">Ver Más...</a>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <hr>
+
+      <!-- Project Three -->
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img\Cartago.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Cartago</h3>
+          <p>La ciudad consta de un centro histórico con 22 avenidas y 26 calles, que se extiende en sentido este-oeste desde el Cementerio General hasta la Basílica de los Ángeles (3 km) y en sentido norte-sur desde la Antigua Comandancia al Nuevo Plantel Municipal (3 km), para un centro histórico de aproximadamente 3 km².</p>
+          <a class="btn btn-primary" href="#">Ver Más...</a>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <hr>
+
+      <!-- Project Four -->
+      <div class="row">
+
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img\Heredia.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Heredia</h3>
+          <p>La colocación de la primera piedra e inicio de la construcción del Templo Parroquial de la Inmaculada en Heredia, data del 31 de octubre de 1797 y según parece, ya prestaba servicio para el año 1806. El terremoto de 1851 el 18 de marzo, destruyó la fachada que luce actualmente.</p>
+          <a class="btn btn-primary" href="#">Ver Más...</a>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <hr>
+
+      <!-- Project Five -->
+      <div class="row">
+
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img\Guanacaste.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Guanacaste</h3>
+          <p>Guanacaste es cuna de muchos aspectos del folclor de Costa Rica, tales como la música, instrumentos como la marimba y el quijongo, danzas tradicionales como el punto guanacasteco, la gastronomía derivada del maíz y del ganado, y la cultura propia de la vida de la hacienda y el campo.</p>
+          <a class="btn btn-primary" href="#">Ver Más...</a>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <hr>      
+
+      <!-- Project Six -->
+      <div class="row">
+
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img\Puntarenas.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Puntarenas</h3>
+          <p>Puntarenas posee varios edificios que han sido declarados patrimonio histórico-arquitectónico de Costa Rica. Entre los más destacados, se encuentra la antigua Capitanía del Puerto de Puntarenas, ubicada al inicio del Paseo de los Turistas.</p>
+          <a class="btn btn-primary" href="Puntarenas.php">Ver Más...</a>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <hr>  
+
+      <!-- Project Seven -->
+      <div class="row">
+
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img\Limon.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Limón</h3>
+          <p>La ciudad de Limón cuenta con varios edificios de estilo victoriano que fueron adaptados al clima húmedo y cálido de la región, dando lugar al estilo arquitectónico victoriano-caribeño. Este estilo se caracteriza por los techos altos con ornamentos de madera a modo de encajes, generosos corredores, torrecillas, barandas y sistemas de ventilación cruzada.</p>
+          <a class="btn btn-primary" href="#">Ver Más...</a>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <hr>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <?php include('footer.php'); ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
