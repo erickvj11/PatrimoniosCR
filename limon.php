d43dbec44c67c32a139bcb794360d6de45e03729
DanPernett <42698453+DanPernett@users.noreply.github.com>
2018-09-21 14:54:43 -0600

    Se agrega limon

---
+++ limon.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <?php include('header.php'); ?>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Half Slider - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/half-slider.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active" style="background-image: url(img/Limon/correosLimon.jpg)">
+            <div class="carousel-caption d-none d-md-block">
+                 <h3>Edificio de Correos y telegrafos de Limon.</h3>
+              <p>Es parte de la historia del desarrollo del puerto y la ciudad de Limón. Es ejemplo de un esfuerzo del desarrollo de nuestros sistemas de comunicación. Es símbolo del espíritu progresista de la comunidad.</p>
+
+              
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('img/Limon/eJustoLimon.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Escuela Justo Antonio Facio de la Guardia</h3>
+              <p> Representa el auge económico de la zona atlántica, y es la principal obra arquitectónica que conforma la plaza de deportes de Siquirres y constituye un marco de integración al paisaje tropical de la zona.</p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('img/Limon/eTomasLimon.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Escuela Tomás Guardia</h3>
+              <p> El General Tomás Guardia es considerado uno de los pilares fundamentales de la consolidación del Estado Costarricense. El 12 de febrero de 1877 se acuerda construir un Instituto Escolar Superior para varones en el Centro de la ciudad de Limón. </p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading -->
+      <h1 class="my-4">Limón
+        <small>Arquitéctónico</small>
+      </h1>
+
+      <div class="row">
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Limon/estacionLimon.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Estación del Ferrocarril al Atlantico</a>
+              </h4>
+              <p class="card-text">El edificio de la Estación del Ferrocarril en Guápiles es de gran valor histórico cultural dentro de esa localidad. Este edificio simboliza el surgimiento de esa población, su desarrollo y sus vivencias como comunidad. desde la época bananera Fue Declarada de Interés Cultural, según Decreto Ejecutivo Nº 23601-C, publicado en La Gaceta Nº 171 del 8 de septiembre de 1994, de carácter estatal</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Limon/casaDiamantes.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Casa de Huéspedes El Diamante</a>
+              </h4>
+              <p class="card-text">Construida a principios del siglo XX, la estructura de madera de laurel y cedro amargo es un ejemplo de la arquitectura típica de las comunidades que se formaron en torno al cultivo del banano en zonas aledañas a las vías del ferrocarril. Fue declarada patrimonio en 1994. La casa de huéspedes se llama así porque durante muchos años se utilizó para alojar a estudiantes y científicos que realizaban sus trabajos de campo en Los Diamantes.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Limon/parqueVargas.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Parque Balvanero Vargas</a>
+              </h4>
+              <p class="card-text">El nombre se debe a que se quiso hacer un homenaje al gobernador que mandaba en aquella época. Está en la calle 32 de la ciudad, a 10 minutos de la playa de los Baños y junto a una cafetería. Se empezó a construir en 1895 tras la donación del terreno donde se encuentra el parque. Era un momento en el que el transporte marítimo permitía traer numerosos árboles tropicales de otros países, como es el caso de las palmeras, crotones, laureles de la india, que permitieron dar al parque un aspecto más tropical.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Limon/blackLimon.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Black Star Line</a>
+              </h4>
+              <p class="card-text">Este inmueble fue construido en el año de 1922 y funcionaba como la sede de la Asociación Universal para el Mejoramiento del Negro (UNIA), creada y liderado por el activista Marcus Garvey. Los miembros de la asociación trabajaron para crear edificios que sirvieran para sus propósitos de justicia social y económica junto con su compañía de vapores “Black Star Line” y es más que una vieja casa de color verde en el centro de Limón, ya que es el edificio más importante para la mayoría de los limonenses y  conserva el recordatorio de la igualdad. ¨. Finalmente se convirtió en un club social, al igual que hicieron otros en la costa del Caribe. Fue declarado patrimonio histórico arquitectónico en el año 2000.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Limon/casaCultura.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Casa de la Cultura</a>
+              </h4>
+              <p class="card-text">En principio, la Casa de la Cultura de Limón fue construida para ser usada como un mercado de carnes. Sin embargo, al finalizar su construcción se utilizó como la Unidad Sanitaria de la ciudad por un espacio de 20 años. Construido en 1939 por el arquitecto costarricense José María Barrantes, es propiedad de la municipalidad de Limón. La Casa de la Cultura, que está ubicada frente al mercado de la ciudad.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Limon/casaRosa.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Casa Misionera (Casa Rosa)</a>
+              </h4>
+              <p class="card-text">La casa misionera está construida de madera de pino de Inglaterra y es un ejemplo de la arquitectura religiosa de la región.  Fue uno de los primeros edificios de dos plantas construidos de madera en la ciudad de Limón y es una reinterpretación del estilo revival gótico,  al estilo tradicional caribeño. Tiene una distribución espacial abierta, que permite la comunicación entre los aposentos con corredores alrededor de ambos niveles, apta para el clima cálido de la zona. Actualmente, esta Casa Rosada es es usada para dar clases de escuela dominical y para reuniones de la Iglesia Bautista.  Fue declarada Patrimonio Arquitectónico en 2002.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <!-- Pagination -->
+      <ul class="pagination justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">1</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">2</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">3</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <?php include('footer.php'); ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
