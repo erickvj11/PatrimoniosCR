33633c90f21a494a2fa90cac5a1b8773bbe75fc1
applebi <42698440+applebi@users.noreply.github.com>
2018-09-22 10:48:40 -0600

    Se agrego Alajuela y sus imagenes

---
+++ alajuela.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <?php include("header.php") ?>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Alajuela</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/half-slider.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+
+        
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active" style="background-image: url('img/alajuela/museo.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Museo Juan Santamaria</h3>
+              <p></p>
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('img/alajuela/escuela5.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Escuela Ascencion Esquivel</h3>
+              <p></p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('img/alajuela/insti.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Antiguo Instituo de Alajuela </h3>
+              <p></p>
+            </div>
+          </div>
+           <!-- Slide four - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('img/alajuela/casabarbie2.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Casa de la cultura</h3>
+              <p></p>
+            </div>
+          </div>
+           
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </header>
+
+    <!--Aqui termina el carrucel de arriba-->
+
+    <!-- Page Content -->
+   <div class="container">
+
+      <!-- Page Heading -->
+      <h1 class="my-4">Alajuela
+        <small>Arquitéctonico</small>
+      </h1>
+
+      <div class="row">
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/alajuela/museo.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Museo Historico Cultual Juan Santamaria</a>
+              </h4>
+              <p class="card-text">Una institución adscrita al Ministerio de Cultura y Juventud. Lleva el nombre "Juan Santamaría" en honor al héroe nacional. Ocupa el edificio de la Antigua Cárcel de Alajuela. Por Ley No. 7895, del 30 de julio de 1999, también le fue asignado el edificio del Antiguo Cuartel de Armas</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/alajuela/insti.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Antiguo Instituo de Alajuela</a>
+              </h4>
+              <p class="card-text">El antiguo Instituo y salon de actos, actual sede de la Universidad Tecnica nacional, fue declarado patrimonio el trese de julio de 1987.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/alajuela/escuela5.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Escuela Ascencion Ibarra</a>
+              </h4>
+              <p class="card-text">La escuela que lleva el nombre del exmandatario, fue declarada patrimonio el cuatro de junio de 1990. Hoy en dia el edificio se encuentra en una restauracion.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/alajuela/casabarbie2.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Casa de la cultura</a>
+              </h4>
+              <p class="card-text">El edificio dela antigua gobernacion, actualmente la casa de la cultura. Ahora casa de programas como CINEM y otros que buscan foentar la cultura en niños y jovenes del canton.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/alajuela/hospital.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Antiguo Hospital San Rafael</a>
+              </h4>
+              <p class="card-text">La fachada sur del Hospital San Rafael de Alajuela, fue declarada patrimonio histórico arquitectónico mediante Decreto Ejecutivo No.28169-C del 3 de setiembre de 1999.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/alajuela/sanjose5.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Templo Católico San José</a>
+              </h4>
+              <p class="card-text">La conocida capilla del Barrio San Jose de Alajuela, es patrimonio arquitefctonico desde el veitne de julio del 2000.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <?php include("footer.php") ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
