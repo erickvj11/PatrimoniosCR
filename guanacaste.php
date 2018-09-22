554b0084cb3e953a6a4ce1182266f9a15396404c
erickvj11 <erickvj11@hotmail.com>
2018-09-22 11:14:00 -0600

    Guanacaste
    Pagina de Guanacaste - Imagenes y PHP

---
+++ guanacaste.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <?php include('header.php'); ?>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Patrimonios Guanacaste</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/half-slider.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active" style="background-image: url(img/Guanacaste/torre.jpg)">
+            <div class="carousel-caption d-none d-md-block">
+                 <h3>Torre del Santuario del Santo Cristo de Esquipulas</h3>
+              <p>La antigua torre se encuentra frente al parque Bernabela Ramos, en el corazón de la ciudad folclórica de Santa Cruz. El viernes 29 de julio del 2005 se declaró la torre como Patrimonio Histórico-Arquitectónico.</p>
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('img/Guanacaste/Ecomuseo.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>EcoMuseo Minero</h3>
+              <p>Es de las primeras edificaciones militares de Costa Rica. El 17 de diciembre de 1998 se declaró como Patrimonio Histórico-Arquitectónico.  La construcción del Cuartel se inició en la administración de Ricardo Jiménez Oreamuno (1932-1936) y se concluyó durante el gobierno de León Cortés Castro (1936-1940).</p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('img/Guanacaste/sanblas.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Iglesia San Blas de Nicoya</h3>
+              <p>Esta estructura es una verdadera joya para los amantes de la arquitectura colonial histórica. El lunes 6 de noviembre de 1995 la Iglesia San Blas fue declarada de interés histórico-arquitectónico.</p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading -->
+      <h1 class="my-4">Guanascaste
+        <small>Arquitéctónica</small>
+      </h1>
+
+      <div class="row">
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Guanacaste/casona.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Casona Santa Rosa</a>
+              </h4>
+              <p class="card-text">Patrimonio de Costa Rica</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Guanacaste/casona_pacifica.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Hacienda La Pacífica</a>
+              </h4>
+              <p class="card-text">Está lleno de historia, podrá experimentar una experiencia típica guanacasteca</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Guanacaste/nicoya.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Iglesia Nicoya</a>
+              </h4>
+              <p class="card-text">Atractivo más relevante</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Guanacaste/comandancia.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Comandacia de Plaza</a>
+              </h4>
+              <p class="card-text">Este inmueble fue construido en el año de 1922 y funcionaba como la sede de la Asociación Universal para el Mejoramiento del Negro (UNIA), creada y liderado por el activista Marcus Garvey. Los miembros de la asociación trabajaron para crear edificios que sirvieran para sus propósitos de justicia social y económica junto con su compañía de vapores “Black Star Line” y es más que una vieja casa de color verde en el centro de Limón, ya que es el edificio más importante para la mayoría de los limonenses y  conserva el recordatorio de la igualdad. ¨. Finalmente se convirtió en un club social, al igual que hicieron otros en la costa del Caribe. Fue declarado patrimonio histórico arquitectónico en el año 2000.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Guanacaste/torre.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Torre del Santuario del Santo Cristo de Esquipulas</a>
+              </h4>
+              <p class="card-text">La antigua torre se encuentra frente al parque Bernabela Ramos, en el corazón de la ciudad folclórica de Santa Cruz. El viernes 29 de julio del 2005 se declaró la torre como Patrimonio Histórico-Arquitectónico.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/Guanacaste/Ecomuseo.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">EcoMuseo Minero</a>
+              </h4>
+              <p class="card-text">Es de las primeras edificaciones militares de Costa Rica. El 17 de diciembre de 1998 se declaró como Patrimonio Histórico-Arquitectónico.  La construcción del Cuartel se inició en la administración de Ricardo Jiménez Oreamuno (1932-1936) y se concluyó durante el gobierno de León Cortés Castro (1936-1940).</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <!-- Pagination -->
+      <ul class="pagination justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">1</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">2</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">3</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <?php include('footer.php'); ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
